31b5f54ff41e5e0990733105bd3d5f11ccdb98f1
danieldorazio <danieldorazio.97@gmail.com>
2023-10-20 17:03:27 +0200

    add: js : class square in html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
+//LOGICA
+
+const rowElem = document.querySelector(".row");
+console.log(rowElem);
+
+let gridString = "";
+
 for (let i = 1; i <= 100; i++ ) {
-    console.log(i);
+    let result;
+
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
+        result = "FizzBuzz"
+        
     } else if (i % 3 === 0) {
         console.log("Fizz");
+        result = "Fizz"
+         
     }else if (i % 5 === 0 ) {
         console.log("Buzz");
+        result = "Buzz"
+
+    } else {
+        console.log(i);
+        result = i
+  
     }
+
+    //console.log(i, result);
+    gridString += `<div class="square">${result}</div>`
+    console.log(gridString);
 }
+
+
+rowElem.innerHTML = gridString;
