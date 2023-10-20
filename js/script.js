b74519a0ce95d344b04828d53cd56c01b74e2863
danieldorazio <danieldorazio.97@gmail.com>
2023-10-20 17:32:01 +0200

    add: style-color

--- js/script.js
+++ js/script.js
@@ -4,30 +4,35 @@ const rowElem = document.querySelector(".row");
 console.log(rowElem);
 
 let gridString = "";
+let bgColor =""
 
 for (let i = 1; i <= 100; i++ ) {
     let result;
 
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
-        result = "FizzBuzz"
+        result = "FizzBuzz";
+        bgColor = "bg-fizz-buzz";
         
     } else if (i % 3 === 0) {
         console.log("Fizz");
-        result = "Fizz"
+        result = "Fizz";
+        bgColor = "bg-fizz";
          
     }else if (i % 5 === 0 ) {
         console.log("Buzz");
         result = "Buzz"
+        bgColor = "bg-buzz"
 
     } else {
         console.log(i);
         result = i
+        bgColor = "bg-number"
   
     }
 
     //console.log(i, result);
-    gridString += `<div class="square">${result}</div>`
+    gridString += `<div class="square ${bgColor}">${result}</div>`
     console.log(gridString);
 }
 
